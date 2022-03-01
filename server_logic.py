90a0e6dc8139edee6f6c59fecb32f735b1e0420c
destructa321 <79952127+Destructa321@users.noreply.github.com>
2022-03-01 21:03:07 +0000

    Added functions to avoid - walls, all snakes on the board

--- server_logic.py
+++ server_logic.py
@@ -33,8 +33,66 @@ def avoid_my_neck(my_head: Dict[str, int], my_body: List[dict], possible_moves: 
 
     return possible_moves
 
+def avoid_the_walls(my_head: dict, board_height: int, board_width: int, possible_moves: List[str]) -> List[str]:
+  
+  if my_head["x"] == 0:  # my head is at the left wall
+      possible_moves.remove("left")
+  elif my_head["x"] == board_width - 1:  # my head is at the right wall
+      possible_moves.remove("right")
+  if my_head["y"] == 0:  # my head is at the bottom wall
+      possible_moves.remove("down")
+  elif my_head["y"] == board_height - 1:  # my head is at the top wall
+      possible_moves.remove("up")
 
-def choose_move(data: dict) -> str:
+  return possible_moves
+
+def avoid_my_body(my_head: Dict[str, int], my_body: List[dict], possible_moves: List[str]) -> List[str]:
+
+  print("----------In body----------")
+  print(my_body)
+  for i in range(2, len(my_body)):
+    if my_body[i]["x"] == my_head["x"] - 1 and my_body[i]["y"] == my_head["y"]:  # current body part is left of my head
+      if "left" in possible_moves:
+        possible_moves.remove("left")
+        print("Removed left")
+    elif my_body[i]["x"] == my_head["x"] + 1 and my_body[i]["y"] == my_head["y"]:  # current body part is right of my head
+      if "right" in possible_moves:
+        possible_moves.remove("right")
+        print("Removed right")
+    elif my_body[i]["y"] == my_head["y"] - 1 and my_body[i]["x"] == my_head["x"]:  # current body part is below my head
+      if "down" in possible_moves:
+        possible_moves.remove("down")
+        print("Removed down")
+    elif my_body[i]["y"] == my_head["y"] + 1 and my_body[i]["x"] == my_head["x"]:  # current body part is above my head
+      if "up" in possible_moves:
+        possible_moves.remove("up")
+        print("Removed up")
+  print("----------")
+  return possible_moves
+
+def avoid_all_snakes(my_head: Dict[str, int], snakes: List[dict], possible_moves: List[str]) -> List[str]:
+
+  for snake in snakes:
+    for i in range(0, len(snake['body'])):
+      if snake['body'][i]["x"] == my_head["x"] - 1 and snake['body'][i]["y"] == my_head["y"]:  # current body part is left of my head
+        if "left" in possible_moves:
+          possible_moves.remove("left")
+          print("Removed left")
+      elif snake['body'][i]["x"] == my_head["x"] + 1 and snake['body'][i]["y"] == my_head["y"]:  # current body part is right of my head
+        if "right" in possible_moves:
+          possible_moves.remove("right")
+          print("Removed right")
+      elif snake['body'][i]["y"] == my_head["y"] - 1 and snake['body'][i]["x"] == my_head["x"]:  # current body part is below my head
+        if "down" in possible_moves:
+          possible_moves.remove("down")
+          print("Removed down")
+      elif snake['body'][i]["y"] == my_head["y"] + 1 and snake['body'][i]["x"] == my_head["x"]:  # current body part is above my head
+        if "up" in possible_moves:
+          possible_moves.remove("up")
+          print("Removed up")
+  return possible_moves
+
+def choose_move(data: dict, board_height: int, board_width: int) -> str:
     """
     data: Dictionary of all Game Board data as received from the Battlesnake Engine.
     For a full example of 'data', see https://docs.battlesnake.com/references/api/sample-move-request
@@ -61,16 +119,19 @@ def choose_move(data: dict) -> str:
     possible_moves = avoid_my_neck(my_head, my_body, possible_moves)
 
     # TODO: Using information from 'data', find the edges of the board and don't let your Battlesnake move beyond them
-    # board_height = ?
-    # board_width = ?
+    possible_moves = avoid_the_walls(my_head, board_height, board_width, possible_moves)
 
     # TODO Using information from 'data', don't let your Battlesnake pick a move that would hit its own body
+    possible_moves = avoid_my_body(my_head, my_body, possible_moves)
 
     # TODO: Using information from 'data', don't let your Battlesnake pick a move that would collide with another Battlesnake
+
+    possible_moves = avoid_all_snakes(my_head, data['board']['snakes'], possible_moves)
 
     # TODO: Using information from 'data', make your Battlesnake move towards a piece of food on the board
 
     # Choose a random direction from the remaining possible_moves to move in, and then return that move
+    #move = possible_moves[0]
     move = random.choice(possible_moves)
     # TODO: Explore new strategies for picking a move that are better than random
 
