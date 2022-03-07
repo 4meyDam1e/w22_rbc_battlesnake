58eeb764c3d12360e68b3173d4b5bb65f74c9a48
Karneet Kaur <67204767+Kaur2112@users.noreply.github.com>
2022-03-06 22:38:37 -0500

    added block_safe function

--- server_logic.py
+++ server_logic.py
@@ -51,30 +51,30 @@ def avoid_the_walls(my_head: dict, board_width: int, board_height: int,
 
 def avoid_my_body(my_head: Dict[str, int], my_body: List[dict],
                   possible_moves: List[str]) -> List[str]:
-    print("----------In body----------")
-    print(my_body)
+    # print("----------In body----------")
+    # print(my_body)
     for i in range(2, len(my_body)):
         if my_body[i]["x"] == my_head["x"] - 1 and my_body[i]["y"] == my_head[
                 "y"]:  # current body part is left of my head
             if "left" in possible_moves:
                 possible_moves.remove("left")
-                print("Removed left")
+                # print("Removed left")
         if my_body[i]["x"] == my_head["x"] + 1 and my_body[i]["y"] == my_head[
                 "y"]:  # current body part is right of my head
             if "right" in possible_moves:
                 possible_moves.remove("right")
-                print("Removed right")
+                # print("Removed right")
         if my_body[i]["y"] == my_head["y"] - 1 and my_body[i]["x"] == my_head[
                 "x"]:  # current body part is below my head
             if "down" in possible_moves:
                 possible_moves.remove("down")
-                print("Removed down")
+                # print("Removed down")
         if my_body[i]["y"] == my_head["y"] + 1 and my_body[i]["x"] == my_head[
                 "x"]:  # current body part is above my head
             if "up" in possible_moves:
                 possible_moves.remove("up")
-                print("Removed up")
-    print("----------")
+                # print("Removed up")
+    # print("----------")
     return possible_moves
 
 
@@ -87,23 +87,23 @@ def avoid_all_snakes(my_head: Dict[str, int], snakes: List[dict],
                         "y"]:  # current body part is left of my head
                 if "left" in possible_moves:
                     possible_moves.remove("left")
-                    print("Removed left")
+                    # print("Removed left")
             if snake['body'][i]["x"] == my_head["x"] + 1 and snake['body'][i][
                     "y"] == my_head[
                         "y"]:  # current body part is right of my head
                 if "right" in possible_moves:
                     possible_moves.remove("right")
-                    print("Removed right")
+                    # print("Removed right")
             if snake['body'][i]["y"] == my_head["y"] - 1 and snake['body'][i][
                     "x"] == my_head["x"]:  # current body part is below my head
                 if "down" in possible_moves:
                     possible_moves.remove("down")
-                    print("Removed down")
+                    # print("Removed down")
             if snake['body'][i]["y"] == my_head["y"] + 1 and snake['body'][i][
                     "x"] == my_head["x"]:  # current body part is above my head
                 if "up" in possible_moves:
                     possible_moves.remove("up")
-                    print("Removed up")
+                    # print("Removed up")
     return possible_moves
 
 
@@ -144,41 +144,112 @@ def move_to_coord(possible_moves: List[str], my_head: Dict[str, int],
     return move
 
 
-def is_safe(coords: List[Dict[str, int]], snakes: List[Dict[str, int]], my_length: int) -> bool:
+def kill_safe(coords: List[Dict[str, int]], snakes: List[Dict[str, int]],
+            my_length: int) -> bool:
     for coord in coords:
         for snake in snakes:
             if (snake['body'][0]['x'] == (coord['x'])
                     and snake['body'][0]['y'] == (coord['y'])):
                 if (len(snake["body"]) >= my_length):
-                    print("SAFE: FALSE")
+                    # print("SAFE: FALSE")
                     return False
-    print("SAFE: TRUE")
+    # print("SAFE: TRUE")
     return True
+
+# def is_safe2(coords: List[Dict[str, int]], board_length: int, board_width: int, my_body: List[Dict[str, int]]) -> bool:
+#     left_of_move = True
+#     right_of_move = True
+#     #checks walls
+#     if (coords[0]["x"] < 0 or coords[0]["x"] > (board_length - 1)
+#             or coords[0]["y"] < 0 or coords[0]["y"] > (board_width - 1)):
+#         left_of_move = False
+        
+
+#     if (coords[1]["x"] < 1 or coords[1]["x"] > (board_length - 1)
+#             or coords[1]["y"] < 0 or coords[1]["y"] > (board_width - 1)):
+#         right_of_move = False
+
+#     for i in my_body:
+#         if coords[0]["x"] == i["x"] and coords[0]["y"] == i["y"]:
+#             left_of_move = False
+#         if coords[1]["x"] == i["x"] and coords[1]["y"] == i["y"]:
+#             right_of_move = False
+
+#     print(f"LEFT OF MOVE: {left_of_move}")
+#     print(f"RIGHT OF MOVE: {right_of_move}")
+#     return left_of_move or right_of_move
+
+def block_safe(coords: List[Dict[str, int]], board_length: int, board_width: int, snakes: List[Dict[str, int]]) -> bool:
+    left_of_move = True
+    right_of_move = True
+    #checks walls
+    if (coords[0]["x"] < 0 or coords[0]["x"] > (board_length - 1)
+            or coords[0]["y"] < 0 or coords[0]["y"] > (board_width - 1)):
+        left_of_move = False
+        
+
+    if (coords[1]["x"] < 1 or coords[1]["x"] > (board_length - 1)
+            or coords[1]["y"] < 0 or coords[1]["y"] > (board_width - 1)):
+        right_of_move = False
+
+    for snake in snakes:
+        for i in snake["body"]:
+            if coords[0]["x"] == i["x"] and coords[0]["y"] == i["y"]:
+                left_of_move = False
+            if coords[1]["x"] == i["x"] and coords[1]["y"] == i["y"]:
+                right_of_move = False
+
+    # print(f"LEFT OF MOVE: {left_of_move}")
+    # print(f"RIGHT OF MOVE: {right_of_move}")
+    return left_of_move or right_of_move
 
 
 def coords_around_move(move: str, my_head: Dict[str,
                                                 int]) -> List[Dict[str, int]]:
     coords = []
-    if (move == "left"):
+    if move == "left":
         next_move = {'x': my_head['x'] - 1, 'y': my_head['y']}
         coords.append({'x': next_move["x"] - 1, 'y': next_move["y"]})  #left
         coords.append({'x': next_move["x"], 'y': next_move["y"] - 1})  #down
         coords.append({'x': next_move["x"], 'y': next_move["y"] + 1})  #up
-    elif (move == "right"):
+    elif move == "right":
         next_move = {'x': my_head['x'] + 1, 'y': my_head['y']}
         coords.append({'x': next_move["x"] + 1, 'y': next_move["y"]})  #right
         coords.append({'x': next_move["x"], 'y': next_move["y"] - 1})  #down
         coords.append({'x': next_move["x"], 'y': next_move["y"] + 1})  #up
-    elif (move == "up"):
+    elif move == "up":
         next_move = {'x': my_head['x'], 'y': my_head['y'] + 1}
         coords.append({'x': next_move["x"] - 1, 'y': next_move["y"]})  #left
         coords.append({'x': next_move["x"] + 1, 'y': next_move["y"]})  #right
         coords.append({'x': next_move["x"], 'y': next_move["y"] + 1})  #up
-    elif (move == "down"):
+    elif move == "down":
         next_move = {'x': my_head['x'], 'y': my_head['y'] - 1}
         coords.append({'x': next_move["x"] - 1, 'y': next_move["y"]})  #left
         coords.append({'x': next_move["x"] + 1, 'y': next_move["y"]})  #right
         coords.append({'x': next_move["x"], 'y': next_move["y"] - 1})  #up
+
+    return coords
+
+
+def blocked_coords(move: str, my_head: Dict[str, int]) -> List[Dict[str, int]]:
+    # print(f"MOVE: {move}")
+    coords = []
+    if move == "left":
+        next_move = {'x': my_head['x'] - 1, 'y': my_head['y']}
+        coords.append({'x': next_move["x"], 'y': next_move["y"] - 1})  #down
+        coords.append({'x': next_move["x"], 'y': next_move["y"] + 1})  #up
+    elif move == "right":
+        next_move = {'x': my_head['x'] + 1, 'y': my_head['y']}
+        coords.append({'x': next_move["x"], 'y': next_move["y"] - 1})  #down
+        coords.append({'x': next_move["x"], 'y': next_move["y"] + 1})  #up
+    elif move == "up":
+        next_move = {'x': my_head['x'], 'y': my_head['y'] + 1}
+        coords.append({'x': next_move["x"] - 1, 'y': next_move["y"]})  #left
+        coords.append({'x': next_move["x"] + 1, 'y': next_move["y"]})  #right
+    elif move == "down":
+        next_move = {'x': my_head['x'], 'y': my_head['y'] - 1}
+        coords.append({'x': next_move["x"] - 1, 'y': next_move["y"]})  #left
+        coords.append({'x': next_move["x"] + 1, 'y': next_move["y"]})  #right
     return coords
 
 
@@ -227,36 +298,59 @@ def choose_move(data: dict) -> str:
     # TODO: Using information from 'data', make your Battlesnake move towards a piece of food on the board
     closest_food = find_closest_food(my_head, board_width, board_height,
                                      data['board']['food'])
-    print("CLOSEST FOOD: ")
-    print(closest_food)
+    # print("CLOSEST FOOD: ")
+    # print(closest_food)
 
     # CHASING TAIL
     if data["you"]["health"] < 50 or my_length < 10:
-      move = move_to_coord(possible_moves, my_head, closest_food)
-    else:
-      move = random.choice(possible_moves)
-    print("POTENTIAL MOVE: " + move)
-
-    risky_moves = []
-    while (len(possible_moves) > 0) and (not is_safe(coords_around_move(move, my_head), data['board']['snakes'], my_length)):
-      # CHASING TAIL
-      risky_moves.append(move)
-      possible_moves.remove(move)
-      if data["you"]["health"] < 50 or my_length < 10:
         move = move_to_coord(possible_moves, my_head, closest_food)
-      else:
+    else:
         move = random.choice(possible_moves)
-      print("POTENTIAL MOVE: " + move)
+    # print("POTENTIAL MOVE: " + move)
+
+    risky_kill_moves = []
+    risky_block_moves = []
+    # print("Initial Move: " + move)
+    # while not is_safe2(blocked_coords(move, my_head), board_height, board_width, my_body):
+    safe_from_kill = kill_safe(coords_around_move(move, my_head), data['board']['snakes'], my_length)
+    safe_from_block = block_safe(blocked_coords(move, my_head), board_height, board_width, data['board']['snakes'])
   
+    while not safe_from_block or not safe_from_kill:
+        # CHASING TAIL
+        # print(f"BLOCK_SAFE: {safe_from_block} KILL_SAFE: {safe_from_kill}")
+        if not safe_from_block:
+          risky_block_moves.append(move)
+        if not safe_from_kill:
+          risky_kill_moves.append(move)
+        possible_moves.remove(move)
+        
+        if len(possible_moves) == 0:
+          if len(risky_kill_moves) == 0:
+            move = random.choice(risky_block_moves)
+            break
+          else:
+            move = random.choice(risky_kill_moves)
+            break
+        
+        if data["you"]["health"] < 50 or my_length < 10:
+            move = move_to_coord(possible_moves, my_head, closest_food)
+        else:
+            move = random.choice(possible_moves)
+        # print("POTENTIAL MOVE: " + move)
+
+        safe_from_kill = kill_safe(coords_around_move(move, my_head), data['board']['snakes'], my_length)
+        safe_from_block = block_safe(blocked_coords(move, my_head), board_height, board_width, data['board']['snakes'])
+
     #coords = coords_around_move(move, my_head)
     #print("COORDS AROUND MOVE: ")
     #print(coords)
     # TODO: Explore new strategies for picking a move that are better than random
-    if len(possible_moves) == 0:
-      move = random.choice(risky_moves)
-    
-    print(
-        f"{data['game']['id']} MOVE {data['turn']}: {move} picked from all valid options in {possible_moves}"
-    )
+
+    # if len(possible_moves) == 0:
+    #   move = random.choice(risky_moves)
+
+    # print(
+    #     f"{data['game']['id']} MOVE {data['turn']}: {move} picked from all valid options in {possible_moves}"
+    # )
 
     return move
